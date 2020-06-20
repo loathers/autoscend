loathers/autoscend

commit e4e4b81f0253685137203ecb8711907a12c79d1d

piratequest fix Frat Boy Ensemble outfit (#438)
* choice adventure 188 make sure you have the means to complete it before run_choice(3) instead of assuming that if you are there and 1 and 2 do not work then you must do 3.
* fix not wearing outfits in pirate quest

RELEASE/scripts/autoscend/quests/optional.ash
@@ -550,7 +550,7 @@ boolean LX_joinPirateCrew() {
 		auto_log_info("Attempting to infiltrate the frat house", "blue");
 		boolean infiltrationReady = false;
 		if (possessOutfit("Frat Boy Ensemble", true))  {
-			outfit("Frat Boy Ensemble");
+			autoOutfit("Frat Boy Ensemble");
 			infiltrationReady = true;
 		} else if (possessEquipment($item[mullet wig]) && item_amount($item[briefcase]) > 0) {
 			autoForceEquip($item[mullet wig]);
@@ -588,7 +588,7 @@ boolean LX_joinPirateCrew() {
 			// this is held together with duct tape and hopes and dreams.
 			// it can and will fail but it will have to do for now.
 			auto_log_info("Beer Pong time.", "blue");
-			outfit("Swashbuckling Getup");
+			autoOutfit("Swashbuckling Getup");
 			backupSetting("choiceAdventure187", "0");
 			tryBeerPong();
 			return true;

RELEASE/scripts/autoscend/auto_choice_adv.ash
@@ -114,9 +114,9 @@ boolean auto_run_choice(int choice, string page)
 				run_choice(1);
 			} else if (equipped_amount($item[mullet wig]) == 1 && item_amount($item[briefcase]) > 0) {
 				run_choice(2);
-			} else {
+			} else if (equipped_amount($item[frilly skirt]) == 1 && item_amount($item[hot wing]) > 2) {
 				run_choice(3);
-			}
+			} else abort("I tried to infiltrate the orcish frat house without being equipped for the job");
 			break;
 		case 189: // O Cap'm, My Cap'm (The Poop Deck)
 			run_choice(2); // skip

commit 26545ff20c3ead98e2a386cd7c2cfbff34f5a518

fix error introduced by #438

RELEASE/scripts/autoscend/quests/optional.ash
@@ -579,7 +579,7 @@ boolean LX_joinPirateCrew() {
 			// this is held together with duct tape and hopes and dreams.
 			// it can and will fail but it will have to do for now.
 			auto_log_info("Beer Pong time.", "blue");
-			autoOutfit("Swashbuckling Getup");
+			outfit("Swashbuckling Getup");	//do not use autoOutfit since we use visit_url in tryBeerPong which skips maximizer
 			backupSetting("choiceAdventure187", "0");
 			tryBeerPong();
 			return true;